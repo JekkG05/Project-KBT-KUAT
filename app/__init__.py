d93c18884a7d3209a747dd040004919d4d1253d8
Jek <zakyg0505@gmail.com>
2026-06-29 00:24:37 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,42 @@
 from flask import Flask
 from flask_login import LoginManager
 
+from app.config import supabase
+from app.models.user import User
+
 
 login_manager = LoginManager()
+
+
+login_manager.login_view = "auth.login"
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+
+    result = (
+        supabase
+        .table("users")
+        .select("*")
+        .eq(
+            "id",
+            int(user_id)
+        )
+        .single()
+        .execute()
+    )
+
+
+    if result.data:
+
+        return User(result.data)
+
+
+    return None
+
+
+
 
 
 def create_app():
@@ -13,28 +47,46 @@ def create_app():
         static_folder="static"
     )
 
-    app.config.from_object("app.config.Config")
+
+    app.config.from_object(
+        "app.config.Config"
+    )
 
 
     login_manager.init_app(app)
 
 
+
     from app.controllers import (
+
         home_bp,
+
         auth_bp,
+
         account_bp,
+
         exercise_bp,
+
         routine_bp,
+
         train_bp
+
     )
 
 
+
     app.register_blueprint(home_bp)
+
     app.register_blueprint(auth_bp)
+
     app.register_blueprint(account_bp)
+
     app.register_blueprint(exercise_bp)
+
     app.register_blueprint(routine_bp)
+
     app.register_blueprint(train_bp)
+
 
 
     return app
