6bafa638298d4b434858c48f7ba1d23f35df18be
Jek <zakyg0505@gmail.com>
2026-06-28 23:56:41 +0700

    Update __init__.py

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,2 +1,6 @@
-# app/controllers/__init__.py
-# Flask blueprint controllers for KUAT.
+from .home_controller import home_bp
+from .auth_controller import auth_bp
+from .account_controller import account_bp
+from .exercise_controller import exercise_bp
+from .routine_controller import routine_bp
+from .train_controller import train_bp
