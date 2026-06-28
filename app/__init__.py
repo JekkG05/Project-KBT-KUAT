bc05bd558e38ca6a906c3ebe68bb3e7a48f53877
Jek <zakyg0505@gmail.com>
2026-06-29 00:53:03 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,41 +1,11 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
-from app.config import supabase
-from app.models.user import User
 
+db = SQLAlchemy()
 
 login_manager = LoginManager()
-
-
-login_manager.login_view = "auth.login"
-
-
-
-@login_manager.user_loader
-def load_user(user_id):
-
-    result = (
-        supabase
-        .table("users")
-        .select("*")
-        .eq(
-            "id",
-            int(user_id)
-        )
-        .single()
-        .execute()
-    )
-
-
-    if result.data:
-
-        return User(result.data)
-
-
-    return None
-
-
 
 
 
@@ -53,40 +23,28 @@ def create_app():
     )
 
 
+    db.init_app(app)
+
+
     login_manager.init_app(app)
 
 
-
     from app.controllers import (
-
         home_bp,
-
         auth_bp,
-
         account_bp,
-
         exercise_bp,
-
         routine_bp,
-
         train_bp
-
     )
 
 
-
     app.register_blueprint(home_bp)
-
     app.register_blueprint(auth_bp)
-
     app.register_blueprint(account_bp)
-
     app.register_blueprint(exercise_bp)
-
     app.register_blueprint(routine_bp)
-
     app.register_blueprint(train_bp)
-
 
 
     return app
