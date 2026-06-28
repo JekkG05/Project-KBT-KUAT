5bedb950edde4e3d506a78de4bed37f752ea1c2e
Jek <zakyg0505@gmail.com>
2026-06-29 00:21:05 +0700

    Update engine_state.py

--- app/models/engine_state.py
+++ app/models/engine_state.py
@@ -1,15 +1,25 @@
 from datetime import datetime
 
-from app import db
 
+class EngineState:
 
-class EngineState(db.Model):
-    __tablename__ = "engine_states"
+    def __init__(self, data):
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, unique=True)
-    state_json = db.Column(db.JSON, nullable=False)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+        self.id = data.get("id")
+
+        self.user_id = data.get("user_id")
+
+        self.state_json = data.get(
+            "state_json",
+            {}
+        )
+
+        self.updated_at = data.get(
+            "updated_at",
+            datetime.utcnow()
+        )
+
 
     def __repr__(self):
+
         return f"<EngineState user_id={self.user_id}>"
