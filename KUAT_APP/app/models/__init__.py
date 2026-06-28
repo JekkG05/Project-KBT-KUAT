e9ad838488b2961f5a17f488c2dedc6f1de6843a
Jek <zakyg0505@gmail.com>
2026-06-28 23:48:38 +0700

    Delete KUAT_APP directory

--- KUAT_APP/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .exercise_db import ExerciseDB
-from .workout_plan import WorkoutPlan
-from .workout_log import WorkoutLog
-from .engine_state import EngineState
-
-__all__ = ["User", "ExerciseDB", "WorkoutPlan", "WorkoutLog", "EngineState"]
