26e32f1d47af2369d3482735f8f543388e2272b2
Jek <zakyg0505@gmail.com>
2026-06-29 00:30:45 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,16 +4,14 @@ from supabase import create_client
 
 class Config:
 
-    SECRET_KEY = os.getenv(
-        "SECRET_KEY"
-    )
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 supabase = create_client(
-
     os.getenv("SUPABASE_URL"),
-
     os.getenv("SUPABASE_KEY")
-
 )
